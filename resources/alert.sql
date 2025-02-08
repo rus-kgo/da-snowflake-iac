0df275a5d57a2fb53e79746e98520ea292588b5d
rusty <rus.kgo@gmail.com>
2025-02-08 20:37:32 +0000

    added alerts

---
+++ resources/alert.sql
@@ -0,0 +1,50 @@
+{% if owner %} 
+USE ROLE {{ owner }}; 
+{%- endif -%}
+{% if database %} 
+USE DATABASE {{ database }}; 
+{%- endif -%}
+{% if schema %} 
+USE SCHEMA {{ schema }}; 
+{%- endif -%}
+
+{% if iac_action.upper() == 'CREATE' %}
+{{ iac_action }} ALERT {{ name }}
+{% if schedule %} SCHEDULE = '{{ schedule }}' {% endif %}
+IF (EXISTS({{ condition }}))
+THEN {{ iac_action }}
+COMMENT = '{"comment":"{{ comment }}", "object_id_tag": "{{ object_id_tag }}"}';
+
+{% if not suspended %}
+ALTER ALERT {{ name }} RESUME;
+{% endif %}
+
+{% elif iac_action.upper() == 'ALTER' and new_name %}
+{{ iac_action }} ALERT {{ old_name }} RENAME TO {{ new_name }};
+
+{% elif iac_action.upper() == 'ALTER' %}
+{% if schedule %}
+{{ iac_action }} ALERT {{ name }} SET SCHEDULE = '{{ schedule }}';
+{% endif %}
+
+{% if suspended %}
+{{ iac_action }} ALERT {{ name }} SUSPEND;
+{% endif %}
+
+{% if not suspended %}
+{{ iac_action }} ALERT {{ name }} RESUME;
+{% endif %}
+
+{% if condition %}
+{{ iac_action }} ALERT {{ name }} MODIFY CONDITION EXISTS ({{ condition }});
+{% endif %}
+
+{% if action %}
+{{ iac_action }} ALERT {{ name }} MODIFY ACTION ({{ action }});
+{% endif %}
+
+
+{% elif iac_action.upper() == 'DROP' %}
+{{ iac_action }} ALERT {{ name }};
+
+{% endif -%}
