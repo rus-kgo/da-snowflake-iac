bc7326ff0c14e07e1c22b6b90e0b3b7d4e365185
rusty <rus.kgo@gmail.com>
2025-03-02 18:18:17 +0000

    added email notification integration

---
+++ resources/notification_integration.sql
@@ -0,0 +1,48 @@
+{% if owner %} 
+USE ROLE {{ owner }}; 
+{%- endif -%}
+
+{% if iac_action.upper() == 'CREATE' %}
+CREATE NOTIFICATION INTEGRATION {{ name }}
+TYPE = EMAIL
+{% if enabled %} ENABLED = {{ enabled | string | upper }} {% endif %}
+{% if allowed_recipients %} ALLOWED_RECIPIENTS = (
+{% for recipient in allowed_recipients %}
+    '{{ recipient }}' {% if not loop.last %}, {% endif %}
+{% endfor %}
+)
+{% endif %}
+{% if default_recipients %} DEFAULT_RECIPIENTS = (
+{% for d_recipient in default_recipients %}
+    '{{ d_recipient }}' {% if not loop.last %}, {% endif %}
+{% endfor %}
+)
+{% endif %}
+{% if default_subject %} DEFAULT_SUBJECT = '{{ default_subject }}'{% endif %}
+COMMENT = '{"comment":"{{ comment }}", "object_id_tag": "{{ object_id_tag }}"}';
+
+{% elif iac_action.upper() == 'ALTER' and new_name %}
+ALTER NOTIFICATION INTEGRATION {{ old_name }} RENAME TO {{ new_name }};
+
+{% elif iac_action.upper() == 'ALTER' %}
+ALTER NOTIFICATION INTEGRATION {{ name }} SET
+{% if enabled %} ENABLED = {{ enabled | string | upper }} {% endif %}
+{% if allowed_recipients %} ALLOWED_RECIPIENTS = (
+{% for recipient in allowed_recipients %}
+    '{{ recipient }}' {% if not loop.last %}, {% endif %}
+{% endfor %}
+)
+{% endif %}
+{% if default_recipients %} DEFAULT_RECIPIENTS = (
+{% for d_recipient in default_recipients %}
+    '{{ d_recipient }}' {% if not loop.last %}, {% endif %}
+{% endfor %}
+)
+{% endif %}
+{% if default_subject %} DEFAULT_SUBJECT = '{{ default_subject }}'{% endif %}
+COMMENT = '{"comment":"{{ comment }}", "object_id_tag": "{{ object_id_tag }}"}';
+
+{% elif iac_action.upper() == 'DROP' %}
+{{ iac_action }} NOTIFICATION INTEGRATION {{ name }};
+
+{% endif -%}
