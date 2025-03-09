287acc2a61b662bdbe0cfde93c307dcd9c4fcf32
rusty <rus.kgo@gmail.com>
2025-03-09 16:59:11 +0000

    added procedures

---
+++ resources/procedure.sql
@@ -0,0 +1,40 @@
+{% if owner %} 
+USE ROLE {{ owner }}; 
+{%- endif -%}
+{% if database %} 
+USE DATABASE {{ database }}; 
+{%- endif -%}
+{% if schema %} 
+USE SCHEMA {{ schema }}; 
+{%- endif -%}
+
+{% if iac_action.upper() == 'CREATE OR REPLACE' %}
+CREATE OR REPLACE PROCEDURE {{ name }}(
+{%if arguments[0].name %}
+{% for a in arguments %}
+    {{a.name}} {{a.type}}{% if not loop.last %}, {% endif %}
+{% endfor %}
+{% endif %}
+)
+RETURNS {{ return_type }}
+LANGUAGE {{ language | upper }}
+{% if runtime_version %}RUNTIME_VERSION = {{ runtime_version }}{% endif %}
+{% if packages %} PACKAGES = (
+{% for p in packages %}
+    '{{ p }}' {% if not loop.last %}, {% endif %}
+{% endfor %}
+)
+{% endif %}
+{% if imports %} IMPORTS = (
+{% for i in imports %}
+    '{{ i }}' {% if not loop.last %}, {% endif %}
+{% endfor %}
+)
+{% endif %}
+{% if handler %}HANDLER = '{{ handler }}'{% endif %}
+{% if body %}AS '''{{ body }}'''{% endif %}
+COMMENT = '{"comment":"{{ comment }}", "object_id_tag": "{{ object_id_tag }}"}';
+
+{% elif iac_action.upper() == 'DROP' %}
+{{ iac_action }} PROCEDURE {{ name }}({{ arguments | map(attribute='type') | join(', ') }});
+{% endif -%}
