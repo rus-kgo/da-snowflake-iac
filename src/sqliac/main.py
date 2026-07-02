f5c37e4e41eadf0469632f1c71db6b37387e91f4
ruskgo <rus.kgo@gmail.com>
2026-07-02 23:41:28 +0100

    fix: main does not re-raise error - cli.py should handle

--- src/sqliac/main.py
+++ src/sqliac/main.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 from sqliac.adapters import AdapterFactory
 from sqliac.constants import IacAction, RunMode
 from sqliac.definitions_loader import DefinitionsLoader
-from sqliac.errors import RustyError
 from sqliac.execution_plan import ExecutionPlan
 from sqliac.providers_loader import ProviderLoader
 from sqliac.scheduler import Scheduler
@@ -13,24 +12,21 @@ from sqliac.scheduler import Scheduler
 
 def run(iac_action: IacAction, run_mode: RunMode) -> None:
     """Entry point of the pipeline."""
-    try:
-        provider_config = ProviderLoader.load()
-        rsc_definitions = DefinitionsLoader.load(provider_config)
-        execution_plan = ExecutionPlan(rsc_definitions).build_execution_plan()
+    provider_config = ProviderLoader.load()
+    rsc_definitions = DefinitionsLoader.load(provider_config)
+    execution_plan = ExecutionPlan(rsc_definitions).build_execution_plan()
 
-        adapter = AdapterFactory.get_adapter(
-            provider=provider_config.name,
+    adapter = AdapterFactory.get_adapter(
+        provider=provider_config.name,
+    )
+
+    with adapter as conn:
+        scheduler = Scheduler(
+            connection=conn,
+            iac_action=iac_action,
+            run_mode=run_mode,
+            provider_resources=provider_config.resources,
+            definitions=rsc_definitions,
         )
 
-        with adapter as conn:
-            scheduler = Scheduler(
-                connection=conn,
-                iac_action=iac_action,
-                run_mode=run_mode,
-                provider_resources=provider_config.resources,
-                definitions=rsc_definitions,
-            )
-
-            scheduler.run(execution_plan=execution_plan)
-    except RustyError as err:
-        print(err)
+        scheduler.run(execution_plan=execution_plan)
